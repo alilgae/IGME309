c69c47f7b0b37be46de63f1a8c17585708d48254
Kaiden Terrana <kaiden.t02@gmail.com>
2023-03-21 19:55:12 -0400

    case 3

--- E08 - Projections/AppClass.cpp
+++ E08 - Projections/AppClass.cpp
@@ -64,6 +64,9 @@ void Application::Display(void)
 		break;
 	case 3:
 		m_pCamera->ResetCamera();
+
+		m_pCamera->SetUp(vector3(0.0f, 0.0f, -1.0f));
+		m_pCamera->SetPosition(vector3(30, 0, 0));
 		break;
 	case 4:
 		m_pCamera->ResetCamera();
